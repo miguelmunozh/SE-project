0efc5a7fcd9b12e47abf57fb83cb4d047f7d6fb8
Jroman5 <jonathan.roman519@gmail.com>
2020-10-09 21:56:45 -0600

    added tests for the systems and logging

--- flaskProject/database/databaseTest.py
+++ flaskProject/database/databaseTest.py
@@ -1,55 +1,83 @@
-from database.databaseHandler import DatabaseHandler
-from database.event import Event, EventType, EventClassification
-from database.analyst import Analyst, Role
+from flaskProject.database.databaseHandler import DatabaseHandler
+from flaskProject.database.event import Event, EventType, EventClassification
+from flaskProject.database.analyst import Analyst, Role
+from flaskProject.database.system import System
 from datetime import date
 from datetime import datetime
 
 dbHandler = DatabaseHandler()
-lists = dbHandler.getAllAnalyst()
-print(lists)
+# lists = dbHandler.getAllAnalyst()
+# initials = []
+# for analyst in lists:
+#     initials.append(analyst.getInitial())
+
+# print(lists)
 
 # analyst = Analyst("jonathan", "roman", "jr", ["jr","sr"], Role.LEAD.value)
-event = Event("eventName",
-              "eventDescription",
-              EventType.VERIFICATION_OF_FIXES.value,
-              "1.0",
-              date(2020, 12, 31).strftime("%m/%d/%Y"),
-              "orgName",
-              "SCTG",
-              EventClassification.UNCLASSIFIED.value,
-              date(2021, 12, 31).strftime("%m/%d/%Y"),
-              "wells Fargo",
-              False,
-              lists)
-dbHandler.updateEvent(event)
+# event = Event("eventName",
+#               "eventDescription",
+#               EventType.VERIFICATION_OF_FIXES.value,
+#               "1.0",
+#               date(2020, 12, 31).strftime("%m/%d/%Y"),
+#               "orgName",
+#               "SCTG",
+#               EventClassification.UNCLASSIFIED.value,
+#               date(2021, 12, 31).strftime("%m/%d/%Y"),
+#               "wells Fargo",
+#               False,
+#               ["jr, cj"])
+#
+# system = System("NAME", "DESC", ["loc1" , "loc2"], ["router1", "router2"], ["switch1", "switch2"], ["room1", "room2"], "testPLAN")
+# dbHandler.updateEvent(analyst, event)
+# dbHandler.updateSystem(analyst, system)
+
+# loglist = dbHandler.getAllLogs()
+# for log in loglist:
+#     print(log.getId())
+#     print(log.getTime().strftime("%m/%d/%Y"))
+#     print(log.getAction())
+#     print(log.getAnalystInitials())
+
+systemList = dbHandler.getAllSystems()
+for system in systemList:
+    print(system.getId())
+    print(system.getName())
+    print(system.getLocation())
+    print(system.getRouter())
+    print(system.getSwitch())
+    print(system.getRoom())
+    print(system.getTestPlan())
+    print(system.getArchiveStatus())
 
 
-analystList = dbHandler.getAllAnalyst()
-eventList = dbHandler.getAllEvents()
+
+
+# analystList = dbHandler.getAllAnalyst()
+# eventList = dbHandler.getAllEvents()
 # shows that there are 3 events in the db
-print(eventList)
-analyst = analystList[0]
-analyst.setFirstName("joe")
+# print(eventList)
+# analyst = analystList[0]
+# analyst.setFirstName("joe")
 # print(analyst.getId())
 # print(analyst.getFirstName())
 # print(analyst.getLastName())
 # print(analyst.getInitial())
 # print(analyst.getRole())
 # print(analyst.getTitle())
-dbHandler.updateAnalyst(analyst)
-event = eventList[0]
-print(event.getName())
-event.setName("test name")
-print(event.getName())
-dbHandler.updateEvent(event)
+# dbHandler.updateAnalyst(analyst)
+# event = eventList[0]
+# print(event.getName())
+# event.setName("test name")
+# print(event.getName())
+# dbHandler.updateEvent(event)
 # print(event.getId())
 # print(event.getName())
-dbHandler.updateEvent(event)
+# dbHandler.updateEvent(event)
 
 # for analyst in analystList:
 #      dbHandler.updateAnalyst(analyst)
 # for event in eventList:
 #     dbHandler.updateEvent(event)
 
-dbHandler.updateEvent(event)
-dbHandler.updateAnalyst(analyst)
+# dbHandler.updateEvent(event)
+# dbHandler.updateAnalyst(analyst)
